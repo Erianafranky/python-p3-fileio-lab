35b4787ff81e2892fc823e7b9e6bfb27a719155e
DorothyAriana <arianadottie@gmail.com>
2023-05-25 11:18:45 +0300

    completed assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
+#write_file
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", mode='w', encoding='utf-8') as text_file:
+        text_file.write(file_content)
 
+#append file
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", mode='a', encoding='utf-8') as text_file:
+        text_file.write(append_content)
 
+#read file
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", encoding='utf-8') as text_file:
+        return text_file.read()
